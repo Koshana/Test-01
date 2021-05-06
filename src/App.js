aa36503da338086ba42bc627c1779e864d2a25e5
Koshana <it18037920@my.sliit.lk>
2021-05-06 09:24:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Koshana from './koshana.js';
+import Hareeni from './hareeni';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Koshana/>
+      <Hareeni/>
     </div>
   );
 }
---
+++ src/koshana.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function koshana() {
+    return (
+        <div>
+            <h1>Koshana's Component</h1>
+        </div>
+    )
+}
+
+export default koshana
